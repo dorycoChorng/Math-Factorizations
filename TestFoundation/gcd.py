7d04ef419a0cf4cfc3650340a41839f1e5be710b
Doryco Chorng <127852657+dorycoChorng@users.noreply.github.com>
2026-03-20 21:59:37 -0700

    Greatest Common Factors
    Greatest common Factor which we started from on solve with 2 variables then 3 and now nth variables

---
+++ TestFoundation/gcd.py
@@ -0,0 +1,52 @@
+'''
+This file is demonstrate of greatest common factor in the mathematics computationally
+*** Theory of the greatest common factor in computer science theory ***
+If A = 0 , so GCD(A,B) = B. If, B = 0; This will be vice versa
+Let say: A is a quotient equation (A = B*y + R); Then GCD(A,B) = GCD(B,R)
+** Using Recursive Method **
+
+'''
+
+import time
+
+# When there is 2 arguements taken
+def gcdTwoArgs(a:int , b:int) -> int:
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        remainder = a % b
+        return gcdTwoArgs(b,remainder)
+    
+
+# When there is 3 Arguements Taken
+def gcdThreeArgs(a:int , b:int , c:int) -> int:
+    if c == 0:
+        gcdTwoArgs(a,b)
+    else:
+        remainders = gcdTwoArgs(a,b) % c
+        return gcdTwoArgs(c,remainders)
+
+
+# Function for solving the body of infinite Greatest Common Factor
+def gcdArguements(numbers, length_Arrays:int) -> int:
+    for n in range (0,length_Arrays):
+        if n == 0:
+            gcdNum = gcdTwoArgs(numbers[n] + numbers[n+1])
+        else:
+            gcdNums = gcdTwoArgs(numbers[n+1] , numbers[n+2])
+            
+    return gcdTwoArgs(gcdNum,gcdNums)
+
+
+
+# When there is nth arguement taken
+def gcd(*numbers) -> int:
+    length_Array = len(numbers)
+    # When the length of the tuples which we don't know how many numbers (If it a even Nums)
+    if length_Array % 2 == 0:
+        gcdArguements(numbers,length_Array)
+    
+    # When it not even
+    else:
