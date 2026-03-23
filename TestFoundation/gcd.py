c04443c6ee006f3ae2bc68df9c1272bab4e85204
Doryco Chorng <127852657+dorycoChorng@users.noreply.github.com>
2026-03-23 01:11:11 +0000

    Update the function to solve the recursion exceeded and to be more effective

--- TestFoundation/gcd.py
+++ TestFoundation/gcd.py
@@ -51,21 +51,46 @@ def gcdArguements(numbers, length_Arrays:int) -> int:
     return gcdTwoArgs(gcdFirst,gcdArguements(others,len(others))) # Recursive again cuz there might be Infinite ones
 
 
+# Function for solving the arguments evenly
+def gcdEven(arrays:list , length:int , anotherArray:list) ->list:
+    # arrays --> The original array
+    # anotherArray --> The another array 
+    i = 0
+    separate = length / 2
+    while i < separate:
+        split = arrays[:2]
+        anotherArray.append(gcdTwoArgs(split[0],split[1]))
 
-
+        listArr = list(arrays) # Convert Tuple to Array
+        listArr.remove(split[0])
+        listArr.remove(split[1])
+        listArr.sort()
+        list(split).clear()
+        i +=1
+    return anotherArray
 
 
 # When there is nth arguement taken
 def gcd(*numbers) -> int:
-    length_Array = len(numbers)
-    # When the length of the tuples which we don't know how many numbers (If it a even Nums)
-    if length_Array % 2 == 0:
-        return gcdArguements(numbers,length_Array)
+    # length_Array = len(numbers)
+    # # When the length of the tuples which we don't know how many numbers (If it a even Nums)
+    # if length_Array % 2 == 0:
+    #     return gcdArguements(numbers,length_Array)
+    # else:
+    # # When it not even
+    #     oddArray = length_Array -1
+    #     return gcdArguements(numbers,length_Arrays=oddArray)
+
+    length = len(numbers)
+    anotherArray = [] # Array on recursive methods
+    if length % 2 == 0:
+        gcdEven(arrays=numbers,length=length,anotherArray=anotherArray)
+        return anotherArray
     else:
-    # When it not even
-        oddArray = length_Array -1
-        return gcdArguements(numbers,length_Arrays=oddArray)
+        lengthsOdd = length -1
+        gcdEven(arrays=numbers,length=lengthsOdd,anotherArray=anotherArray)
+        anotherArray.append(numbers[len(numbers) - 1]) # end index
 
 
 
-print(gcd(9,36))
+print(gcd(8,12,12,45,78,234,12))
