24d10d6a652f5de76d27d57440c3ce4974cd054c
Doryco Chorng <127852657+dorycoChorng@users.noreply.github.com>
2026-03-31 01:24:34 +0000

    Comments explain

--- TestFoundation/array/separation/separationArrPack.py
+++ TestFoundation/array/separation/separationArrPack.py
@@ -9,16 +9,19 @@ gcds = gcdpkg.gcds()
 
 class separationArray:
     def __init__(self, array: list):
-        self.array = list(array)
+        self.array = list(array) # Make the array self in the class
 
     def separation(self) -> list:
+        # Variable
         arrays = list(self.array)
         newArr = []
         length = len(arrays)
 
+        # When the array length is even
+
         if length % 2 == 0:
             i = 0
-            while i < length // 2:
+            while i < length / 2:
                 split = arrays[:2]
                 newArr.append(gcds.gcdTwoArgs(split[0], split[1]))
                 arrays.remove(split[0])
@@ -26,6 +29,7 @@ class separationArray:
                 arrays.sort()
                 i += 1
 
+            # If it still large and do another recursive for simple terms
             if len(newArr) > 2:
                 return separationArray(newArr).separation()
             elif len(newArr) == 1:
@@ -35,15 +39,18 @@ class separationArray:
                 arrays = [gcD]
                 return arrays
 
+    # When the array length is odd
+
         else:
-            endIndex = arrays[-1]
-            firstArray = arrays[:-1]
-            firstIndexes = separationArray(firstArray).separation()
-            firstIndexes.append(endIndex)
+            endIndex = arrays[-1] # Value of the last index
+            somearr = arrays[:-1] # Remove the last index
+            firstIndexes = separationArray(somearr).separation() # Put the remain indexes to recursive
+            firstIndexes.append(endIndex) # Add it back for final touch
             arrays = [gcds.gcdTwoArgs(firstIndexes[0], firstIndexes[1])]
             return arrays
 
 
 # Print
-main = separationArray([11, 121]).separation()
+main = separationArray([8,4,36,12,72,42,52]).separation()
 print(main)
+
