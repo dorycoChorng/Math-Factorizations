3a63270fb8b23761b2960a8e771df9dc55a93e8b
Doryco Chorng <127852657+dorycoChorng@users.noreply.github.com>
2026-03-31 01:15:30 +0000

    Import the class & PyCache

--- TestFoundation/array/separation/separationArrPack.py
+++ TestFoundation/array/separation/separationArrPack.py
@@ -1,49 +1,49 @@
-# impport
-from TestFoundation import gcdPackage
+import os
+import sys
+
+# Ensure root package path is on sys.path when executing from nested folder.
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+
+import gcdPackage as gcdpkg
+gcds = gcdpkg.gcds()
+
 class separationArray:
-    def _init_(self,array:list):
-        self.array = array
+    def __init__(self, array: list):
+        self.array = list(array)
 
     def separation(self) -> list:
-        arrays = self.array
+        arrays = list(self.array)
+        newArr = []
         length = len(arrays)
+
         if length % 2 == 0:
             i = 0
-            while i < length/2:
+            while i < length // 2:
                 split = arrays[:2]
-                newArr.append(gcds.gcdTwoArgs(split[0],split[1])) # The gcd of 2 nums to append in the new array
-                arrays.remove(split[0]) 
+                newArr.append(gcds.gcdTwoArgs(split[0], split[1]))
+                arrays.remove(split[0])
                 arrays.remove(split[1])
                 arrays.sort()
                 i += 1
-            
+
             if len(newArr) > 2:
-                arrays.clear()
-                arrays = newArr.copy()
-                newArr.clear()
-                return separation(arrays)
+                return separationArray(newArr).separation()
             elif len(newArr) == 1:
                 return newArr
             else:
-                gcD = gcds.gcdTwoArgs(newArr[0],newArr[1])
-                arrays.clear()
-                arrays.append(gcD)
+                gcD = gcds.gcdTwoArgs(newArr[0], newArr[1])
+                arrays = [gcD]
                 return arrays
-        
-        # when odd
+
         else:
-            lens = (length-1)/2 # The length for odd
-            endIndex = arrays[length-1]
-            firstArray = arrays.copy()
-            firstArray.remove(endIndex)
-            firstIndexes = separation(firstArray)
+            endIndex = arrays[-1]
+            firstArray = arrays[:-1]
+            firstIndexes = separationArray(firstArray).separation()
             firstIndexes.append(endIndex)
-            arrays.clear()
-            arrays.append(gcds.gcdTwoArgs(firstIndexes[0],firstIndexes[1]))
+            arrays = [gcds.gcdTwoArgs(firstIndexes[0], firstIndexes[1])]
             return arrays
-    
+
 
 # Print
-
-main = separationArray.separation([8,9])
+main = separationArray([11, 121]).separation()
 print(main)
